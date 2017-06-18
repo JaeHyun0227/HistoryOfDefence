a8c330854544ea396b662e613b727ebd762d8834
JaeHyun0227 <gus7470@gmail.com>
2017-06-18 23:24:31 +0900

    HistoryOfDefence

---
+++ Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public int i = 0;
+
+    public static UIManager instance;
+
+    void Awake() { instance = this; }
+
+    
+
+
+    public void Scene_Home()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void Scene_Shop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+}
