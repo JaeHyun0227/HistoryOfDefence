54b69d5dca61da0386aead47e7ec712566ece0e9
JaeHyun0227 <gus7470@gmail.com>
2017-06-28 16:50:54 +0900

    Add files via upload

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,22 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
-    public int i = 0;
+    public float time = 0;
+    public char direction;
+
+    public GameObject RightOver,LeftOver;
 
     public static UIManager instance;
 
+
+
     void Awake() { instance = this; }
 
-    
 
-
-    public void Scene_Home()
-    {
-        SceneManager.LoadScene("Home");
+    public void SceneMove(string SceneName){
+         SceneManager.LoadScene(SceneName);
     }
-
-    public void Scene_Shop()
+    public void CameraMove(Camera camera)
     {
-        SceneManager.LoadScene("Shop");
+            switch (direction)
+            {
+                case 'R': camera.transform.position = Vector3.Lerp(camera.transform.position, RightOver.transform.position, time); break;
+                case 'L': camera.transform.position = Vector3.Lerp(camera.transform.position, LeftOver.transform.position, time); break;
+                default: break;
+            }
+            time += Time.deltaTime * 0.1f;
+
     }
 }
